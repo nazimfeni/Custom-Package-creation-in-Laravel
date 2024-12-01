6979feaf5169bcd394aca7e43f18b3119ca247fb
nazimjrf <nazim@jrfintel.com>
2024-12-01 13:16:32 +0600

    ff

--- package/contact/src/Http/Controllers/ContactController.php
+++ package/contact/src/Http/Controllers/ContactController.php
@@ -11,14 +11,16 @@ use Bionictechbd\Contact\Mail\ContactMailable;
 class ContactController extends Controller
 {
    public function index(){
+
     return view('contact::contact');
+    //return view('contact::test');
    }
 
    public function send(Request $request){
-    
+
       Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message, $request->name));
       Contact::create($request->all());
-    
+
       return redirect(route('contact'));
    }
 }
